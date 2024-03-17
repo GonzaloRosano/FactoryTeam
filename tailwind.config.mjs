f5147338d74e07b7c4cde50316fe895d51c5af39
Gonzalo Rosano <rosanogonzalo@gmail.com>
2024-03-17 07:33:36 -0300

    implementacion de resposive para MemberCard.astro e index.astro

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,14 @@ export default {
         mc: ["Minecraft"],
       },
     },
+    screens: {
+      xs: "470px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    plugins: [],
   },
-  plugins: [],
 };
